068dc537d0a580281bb8d03536ab3fee1f068b7d
Anton Suprunchuk <antouhou@gmail.com>
2020-08-31 20:13:28 +0300

    Add some echoes

--- abci/tests/test_app/test.sh
+++ abci/tests/test_app/test.sh
@@ -18,12 +18,14 @@ cd "$DIR"
 
 echo "RUN COUNTER OVER SOCKET"
 # test golang counter
+echo "CGO_CXXFLAGS="$CGO_CXXFLAGS" CGO_LDFLAGS="$CGO_LDFLAGS" ABCI_APP="counter" go run -mod=readonly ./*.go"
 CGO_CXXFLAGS="$CGO_CXXFLAGS" CGO_LDFLAGS="$CGO_LDFLAGS" ABCI_APP="counter" go run -mod=readonly ./*.go
 echo "----------------------"
 
 
 echo "RUN COUNTER OVER GRPC"
 # test golang counter via grpc
+echo "CGO_CXXFLAGS="$CGO_CXXFLAGS" CGO_LDFLAGS="$CGO_LDFLAGS" ABCI_APP="counter --abci=grpc" ABCI="grpc" go run -mod=readonly ./*.go"
 CGO_CXXFLAGS="$CGO_CXXFLAGS" CGO_LDFLAGS="$CGO_LDFLAGS" ABCI_APP="counter --abci=grpc" ABCI="grpc" go run -mod=readonly ./*.go
 echo "----------------------"
 
