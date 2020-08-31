6abbb97fc71324f9d91130d59cb13a888b2ecaf9
Anton Suprunchuk <antouhou@gmail.com>
2020-08-31 16:43:18 +0300

    Change the path one more time

--- scripts/build_bls_library.sh
+++ scripts/build_bls_library.sh
@@ -1,11 +1,15 @@
-rm -rf "${GOPATH}"/src/github.com/quantumexplorer/bls-signatures
-mkdir -pv "${GOPATH}"/src/github.com/quantumexplorer/
-git clone https://github.com/quantumexplorer/bls-signatures.git "${GOPATH}"/src/github.com/quantumexplorer/bls-signatures
-cd "${GOPATH}"/src/github.com/quantumexplorer/bls-signatures
+BLS_REPO_PATH="${GOPATH}"/github.com/quantumexplorer/bls-signatures
+
+rm -rf "${BLS_REPO_PATH}"
+
+mkdir -pv "${GOPATH}"/github.com/quantumexplorer/
+
+git clone https://github.com/quantumexplorer/bls-signatures.git "$BLS_REPO_PATH"
+cd "$BLS_REPO_PATH"
 git submodule update --init --recursive
 mkdir build
 cd build
 cmake ../
 cmake --build . -- -j 6
-cd "${GOPATH}"/src/bls-signatures/go-bindings
+cd "${BLS_REPO_PATH}"/go-bindings
 make
