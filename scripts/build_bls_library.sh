629dcf51a50c88bd34ef669564f8ac9aa54eaad2
Anton Suprunchuk <antouhou@gmail.com>
2020-08-31 15:45:57 +0300

    Add build script for the bls lib

---
+++ scripts/build_bls_library.sh
@@ -0,0 +1,11 @@
+rm -rf $GOPATH/src/github.com/quantumexplorer/bls-signatures
+mkdir -pv $GOPATH/src/github.com/quantumexplorer/
+git clone https://github.com/quantumexplorer/bls-signatures.git $GOPATH/src/github.com/quantumexplorer/bls-signatures
+cd $GOPATH/src/github.com/quantumexplorer/bls-signatures
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake ../
+cmake --build . -- -j 6
+cd bls-signatures/go-bindings
+make
