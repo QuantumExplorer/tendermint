00bb1ee1cb870f7e0e56b74682bc9d7d38aa2ac6
Anton Suprunchuk <antouhou@gmail.com>
2020-08-31 17:29:56 +0300

    Add some debug output

--- scripts/build_bls_library.sh
+++ scripts/build_bls_library.sh
@@ -1,12 +1,19 @@
+GOPATH=${BUILD_DIR}/gopath
 BLS_REPO_PATH="${GOPATH}"/github.com/quantumexplorer/bls-signatures
 
-rm -rf "${BLS_REPO_PATH}"
+echo $(go env GOPATH)
+echo ${PWD}
 
+# Cleaning previous build
+rm -rf "${BLS_REPO_PATH}"
 mkdir -pv "${GOPATH}"/github.com/quantumexplorer/
 
+# Cloning bls repo and fetching dependencies
 git clone https://github.com/quantumexplorer/bls-signatures.git "$BLS_REPO_PATH"
 cd "$BLS_REPO_PATH"
 git submodule update --init --recursive
+
+# Build the bindings
 mkdir build
 cd build
 cmake ../
